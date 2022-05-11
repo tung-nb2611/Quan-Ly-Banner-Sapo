62992fd0327d5b6578d0afd9781d66545d07c69d
tungnb <tung.nb2611@gmail.com>
2022-05-11 11:23:25 +0700

    code moi nhat

--- frontend/src/components/report/DetailClick.js
+++ frontend/src/components/report/DetailClick.js
@@ -3,12 +3,12 @@ const DetailClick = ({ clickInfo }) => {
     return (
         <tr>
             <td>{clickInfo.userClick}</td>
-<<<<<<< HEAD
+
             <td>{clickInfo.sectionID}</td>
-=======
+
             <td>{clickInfo.timeClick}</td>
 
->>>>>>> b7f381cbccc2a2a7baf6cfb3c24a6d596be19662
+
         </tr>
     )
 };
