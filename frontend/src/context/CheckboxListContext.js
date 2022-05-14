bbb8f4f7eaf79317caf1bea8552e085a745f5eb7
duongbx288 <duongbx183901@gmail.com>
2022-05-14 11:16:03 +0700

    92% checkbox done - only need to rerender the section-banner manage when press the 'save' button

--- frontend/src/context/CheckboxListContext.js
+++ frontend/src/context/CheckboxListContext.js
@@ -1,44 +1,74 @@
 import { createContext, useState } from "react";
 
-
-
-// Sử dụng để lưu các checkbox đã được tích 
-// để khi chuyển trang (ở phần phân trang) thì 
-// các items đã được tích sẽ vẫn giữ nguyên hiện trạng 
+// Sử dụng để lưu các checkbox đã được tích
+// để khi chuyển trang (ở phần phân trang) thì
+// các items đã được tích sẽ vẫn giữ nguyên hiện trạng
 // đồng thời lưu lại tỉ trọng của từng banner
 const CheckboxArrContext = createContext();
 
 const CheckboxArrProvider = ({ children }) => {
-    const [countArr, setCountArr] = useState([]);
+  const [countArr, setCountArr] = useState([]);
+  const [bannerArr, setBannerArr] = useState([]);
 
-    const setCheckboxObject = (value) => {
-        setCountArr(state => [...state, value]);
-    }
+  const setCheckboxObject = (value) => {
+    setCountArr((state) => [...state, value]);
+  };
 
-    const removeArrItem = (idNumber) => {
-        setCountArr(countArr.filter(item => {
-            return item.id !== idNumber
-        }));
-    }
+  const removeArrItem = (idNumber) => {
+    setCountArr(
+      countArr.filter((item) => {
+        return item.id !== idNumber;
+      })
+    );
+  };
 
-    const updateCheckboxArr = (id, value) => {
-        const tempArr = countArr.map(banner => banner.id === id ? { ...banner, rate: value } : banner);
-        setCountArr(tempArr);
-    }
+  console.log(bannerArr);
 
+  const updateCheckboxArr = (id, value) => {
+    const tempArr = countArr.map((banner) =>
+      banner.id === id ? { ...banner, percentage: value } : banner
+    );
+    setCountArr(tempArr);
+  };
 
-    const checkboxArray = {
-        countArr,
-        setCheckboxObject,
-        updateCheckboxArr,
-        removeArrItem
-    }
+  //
+  const addBannerArr = (bannerInfo) => {
+    console.log(bannerInfo);
+    const tempInfo = {...bannerInfo, state: 1}
+    setBannerArr((state) => [...state, bannerInfo]);
+  }
 
-    return (
-        <CheckboxArrContext.Provider value={checkboxArray}>
-            {children}
-        </CheckboxArrContext.Provider>
-    )
-}
+  const updateBannerArr = (id, value) => {
+    const tempArr = bannerArr.map((banner) =>
+      banner.id === id ? { ...banner, percentage: value } : banner
+    );
+    setBannerArr(tempArr);
+  };
+
+  const updateBannerState = (id, state) => {
+    const tempArr = bannerArr.map((banner) =>
+      banner.id === id ? { ...banner, state: state } : banner
+    );
+    setBannerArr(tempArr);
+  };
+
+  const checkboxArray = {
+    countArr,
+    setCheckboxObject,
+    updateCheckboxArr,
+    removeArrItem,
+    bannerArr,
+    updateBannerArr,
+    updateBannerState,
+    setBannerArr,
+    addBannerArr
+  };
+
+  return (
+    <CheckboxArrContext.Provider value={checkboxArray}>
+      {children}
+    </CheckboxArrContext.Provider>
+  );
+};
 
 export { CheckboxArrProvider, CheckboxArrContext };
