ee7a0abc73bf0daccb4ea2cb2c6a4708e52e53ea
Le Ngoc Kien <lengockien2000@gmail.com>
2022-05-15 00:00:11 +0700

    update css

--- frontend/src/components/website/Website.js
+++ frontend/src/components/website/Website.js
@@ -1,30 +1,22 @@
-import { useBootstrapBreakpoints } from "react-bootstrap/esm/ThemeProvider";
 import { Link } from "react-router-dom";
 import "../../styles/section/Section.css";
-import { Row, Col } from "react-bootstrap";
 
 function Website({ data }) {
   return (
-
-    <div>
-      <div className="banner-info m-4 p-3">
-        <Row>
-          <Col className="detail-info">
-            <label>Section:{data.id}</label>
-          </Col>
-          <Col className="detail-info">
-            <label>Web name: {data.name}</label>
-          </Col>
-          <Col className="detail-info">
-            <label>Url: {data.url}</label>
-          </Col>
-          <Col>
-            <Link to={"/websites/websiteId=" + data.id + "/sections"}>
-              <button>Các khu vực banner của trang web</button>
-            </Link>
-          </Col>
-        </Row>
-
+    <div className="section-info p-3">
+      <div className="row">
+        <div className="detail-info col-12 col-sm-8">
+          <label> Section:{data.id} </label>
+          <label> Web name: {data.name} </label>
+          <label> Url: {data.url} </label>
+        </div>
+        <div className="button col-12 col-sm-4">
+          <Link to={"/websites/websiteId=" + data.id + "/sections"}>
+            <button className="section">
+              Các khu vực banner của trang web
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/website/WebsiteList.js
+++ frontend/src/components/website/WebsiteList.js
@@ -13,50 +13,53 @@ function WebsiteList(props) {
     const [pageNumber, setPageNumber] = useState(0);
     const [currentPage, setCurrentPage] = useState(0);
 
+    let user = JSON.parse(window.localStorage.getItem("user"));
     useEffect(() => {
-
-        WebsiteService.getWebsiteByPageAndUserAdd("tung", currentPage).then((response) => {
-
-
-
-
-            const info = response.data.content;
-            const pageNum = response.data.totalPages;
-            setWebsiteList(info);
-            setPageNumber(pageNum);
-        })
-    }, [currentPage])
+        if (user.roles === '["ROLE_USER"]') {
+            WebsiteService.getWebsiteByPageAndUserAdd(user.username, currentPage).then((response) => {
+                const info = response.data.content;
+                const pageNum = response.data.totalPages;
+                setWebsiteList(info);
+                setPageNumber(pageNum);
+            })
+        } else {
+            WebsiteService.getWebsiteByPage(currentPage).then((response) => {
+                const info = response.data.content;
+                const pageNum = response.data.totalPages;
+                setWebsiteList(info);
+                setPageNumber(pageNum);
+            }
+            )
+        }
+    }, [user, currentPage])
 
     const displayWebsites = websiteList.map(
         (data) => {
             return (
-                <div key={data.id}>
+                <div className="col-md-12 col-lg-6 mb-3" key={data.id}>
                     <Website data={data} />
-
                 </div>
             )
         }
     )
 
     return (
-        <div className="banner-list m-2">
-            <div className="list">
+        <div className="section-list p-3">
+            <h2>
+                Danh sách các Websites
+            </h2>
+            <hr/>
+            <Link to={"/website/create"}>
+                <button className="section-create p-2">
+                    <h4>Thêm mới website</h4>
+                </button>
+            </Link>
+            <div className="list d-flex row mt-3">
                 {displayWebsites}
-                <Link to={"/website/create"}>
-                    <button className="section">
-                        <h4>Thêm mới website</h4>
-                    </button>
-                </Link>
             </div>
             <PaginateList currentPage={currentPage} setCurrentPage={setCurrentPage} pageNumber={pageNumber} />
-
         </div>
     )
 }
-
-
-
-
-
 
 export default WebsiteList;
