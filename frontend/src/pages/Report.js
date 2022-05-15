f2670761b80e54d4daf68f329065f3ef24961a92
tungnb <tung.nb2611@gmail.com>
2022-05-15 16:22:32 +0700

    add report wiget

--- frontend/src/pages/Report.js
+++ frontend/src/pages/Report.js
@@ -5,27 +5,32 @@ import ListBannerInSection from "../components/report/ListBannerInSection";
 import ListBannerReport from "../components/report/ListBannerReport";
 import Views from "../components/report/Views";
 import * as BiIcons from "react-icons/bi";
+import Widget from "../components/widget/Widget"
 
 const Report = () => {
     // const [currentTime, setCurrentTime] = Date.now();
     const current = new Date();
-    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+    const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
     return (
         <div className="p-3 d-flex flex-column gap-4">
             <div className="header d-flex justify-content-between">
                 <div className="d-inline-flex gap-3">
                     <h3>WEBSITE</h3>
-                    <BiIcons.BiRightArrow className="mt-2"/>
+                    <BiIcons.BiRightArrow className="mt-2" />
                     <ChoiceSection />
                 </div>
                 <h3>{date}</h3>
             </div>
-            
+            <div>
+                <Widget />
+
+
+            </div>
             <div>
                 <Views />
             </div>
             <div>
-                {/* <ListBannerReport /> */}
+                <ListBannerReport />
             </div>
             <div>
                 {/* <ListBannerInSection /> */}
