f2161700935870132f83c2118a33479350c7ab04
duongbx288 <duongbx183901@gmail.com>
2022-05-16 16:29:17 +0700

    Sửa phần select box không hiện theo đúng thông tin webid & sectionId

--- frontend/src/components/section/Section.js
+++ frontend/src/components/section/Section.js
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import "../../styles/section/Section.css"
 
 function Section({ data }) {
+
+  const webId = data.webId;
+
   return (
     <div className="section-info p-3">
       <div className="row">
@@ -11,7 +14,7 @@ function Section({ data }) {
           <label> Div: <span>{data.divId}</span></label>
         </div>
         <div className="button col-12 col-sm-4">
-          <Link to={"/banner/display/" + data.id}>
+          <Link to={{pathname: "/banner/display/" + data.id, state: {webId}}}>
             <button className="section">
               Quản lý hiển thị banner
             </button>
